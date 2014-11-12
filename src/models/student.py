9b23d7ba03a609481f923de50ba075e2d2e9af64
Aaron <aaron.nahum@hotmail.com>
2014-11-12 12:39:48 -0500

    Added passwordHash to Student.py
    Also fixed file name

--- src/models/student.py
+++ src/models/student.py
@@ -1,7 +1,8 @@
 class Student:
 
-    def __init__(self, studentId, grade, selectedCourses, availableCourses):
+    def __init__(self, studentId, grade, selectedCourses, availableCourses, passwordHash):
         self.studentId = studentId
         self.grade = grade
         self.selectedCourses = selectedCourses
         self.availableCourses = availableCourses
+        self.passwordHash = passwordHash
