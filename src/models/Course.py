62ea7008e6bf6cde786cc3b0a4d059054b62bdf2
Aaron <aaron.nahum@hotmail.com>
2014-11-12 12:30:06 -0500

    Added student and course models

---
+++ src/models/Course.py
@@ -0,0 +1,6 @@
+class Course:
+
+    def __init__(self, courseId, name, priority):
+        self.courseId = courseId
+        self.name = name
+        self.priority = priority
---
+++ src/models/student.py
@@ -0,0 +1,7 @@
+class Student:
+
+    def __init__(self, studentId, grade, selectedCourses, availableCourses):
+        self.studentId = studentId
+        self.grade = grade
+        self.selectedCourses = selectedCourses
+        self.availableCourses = availableCourses
