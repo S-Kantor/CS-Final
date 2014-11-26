b57fe9d1bdc1702e6db467333b0b4d77d2e080be
Express50 <press.ex50@gmail.com>
2014-11-26 11:58:27 -0500

    Start working on CreateAccountFrame in Login.py

--- src/ui/Login.py
+++ src/ui/Login.py
@@ -43,17 +43,48 @@ class LoginFrame(Frame):
         loginButton = Button(self, text="Login", command = self.login)
         loginButton.grid(row=5, column=0)
 
-    def login (self):
-        self.Main_Screen = Main_Screen()
+    def login(self):
+        return
 
-class Main_Screen(Frame):     
-    def __init__(self):
-        new = Frame.__init__(self)
-        new = Toplevel(self)
-        new.title("Main Screen")
+class CreateAccountFrame(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.parent = parent
+        self.initUI()l
 
-    def close_window(self):
-        self.destroy()        
+    def initUI(self):
+        self.parent.title("Create an Account")
+        self.pack(fill=BOTH, expand=1)
+
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
+        self.rowconfigure(2, weight=1)
+        self.rowconfigure(3, weight=1)
+        self.rowconfigure(4, weight=1)
+        self.rowconfigure(5, weight=1)
+        self.rowconfigure(6, weight=1)
+
+        createAccountLabel = Label(self, text="Create an Account", font=(16))
+        createAccountLabel.grid(row=0, column=0, sticky=S)
+
+        idLabel = Label(self, text="Student ID:")
+        idLabel.grid(row=1, column=0, sticky=S)
+        idText = Entry(self)
+        idText.grid(row=2, column=0)
+
+        passwordLabel = Label(self, text="Password:")
+        passwordLabel.grid(row=3, column=0, sticky=S)
+        passwordText = Entry(self)
+        passwordText.grid(row=4, column=0)
+
+        gradeLabel = Label(self, text="Grade:")
+        gradeLabel.grid(row=5, column=0, sticky=S)
+
+        variable = StringVar(master)
+        variable.set("9")
+        gradeSelection = OptionMenu(self, variable, "9", "10", "11", "12")
+        gradeSelection.grid(row=6, column=0, sticky=S)
     
 def main():
     root = Tk()
--- src/ui/Main.py
+++ src/ui/Main.py
@@ -13,51 +13,23 @@ class MainFrame(Frame):
 
         self.parent = parent
 
-        self.gridconfig()
-
         self.initUI()
 
-    def gridconfig(self):
-
-        #Config Column 1 and rows for column one
-
-        self.columnconfigure(0, weight=3)
-        self.rowconfigure(0, weight=1)
-        self.rowconfigure(1, weight=1)
-        self.rowconfigure(2, weight=1)
-        self.rowconfigure(3, weight=1)
-        self.rowconfigure(4, weight=1)
-        self.rowconfigure(5, weight=1)
-
-        #Config Column 2 and rows for column two
-
-        self.columnconfigure(1, weight=1)
-        self.rowconfigure(0, weight=3)
-        self.rowconfigure(1, weight=1)
-        self.rowconfigure(2, weight=1)
-        self.rowconfigure(3, weight=1)
-        self.rowconfigure(4, weight=1)
-        self.rowconfigure(5, weight=1) 
-
-        
-
     def initUI(self):
-        
-    
         self.parent.title("Main Screen")
-        self.pack(fill=BOTH, expand=1)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.grid()
 
-        scrollbar = Scrollbar(self, orient=VERTICAL)
-        listbox = Listbox(self, yscrollcommand=scrollbar.set)
-        scrollbar.config(command=listbox.yview)
-        scrollbar.pack(side=RIGHT, fill=Y)
-        listbox.pack(side=LEFT, fill=BOTH, expand=1)
+        self.available = Listbox(self)
+        self.available.grid(row=0, column=0, rowspan=2)
 
-    def addCourse(self):
-        print ("Do nothing")
+        self.selected = Listbox(self)
+        self.selected.grid(row=0, column=1, rowspan=2)
 
-
-
+        for i in range(100):
+            self.available.insert(END, str(i))
+        
 def main():
     root = Tk()
     root.geometry("450x400+400+400")
