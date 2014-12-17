c878f78f022a014fa0f3708dd5d79baa27b66e05
Aaron <aaron.nahum@hotmail.com>
2014-12-17 12:36:39 -0500

    Added a chooseCourse function
    The function moves a selected course into a students list of courses.

--- src/Main.py
+++ src/Main.py
@@ -8,6 +8,7 @@
 from tkinter import *
 from SQLWrapper import *
 s = SQLWrapper()
+selected_courses = []
 
 class MainFrame(Frame):
     def __init__(self, parent):
@@ -28,39 +29,47 @@ class MainFrame(Frame):
 
         #####
         
-        Box1ID = Label(self, text="Available Courses:")
-        Box1ID.grid(row=1, column=0, sticky=S)
-        
-        scroller1 = Scrollbar(self, orient="vertical")
-        Box1 = Listbox(self, width=30, height=20, yscrollcommand=scroller1.set)
-        scroller1.config(command=Box1.yview)
-        for course in s.getAllCourses():
-            Box1.insert(END, course.name)
-
-        Box1.grid(row=2, column=0)
-        scroller1.grid(sticky=E, row = 2, rowspan = 100, column = 1, ipady = 138)
-
-        loginButton = Button(self, text="Choose Course")
-        loginButton.grid(row=3, column=0)
-        
-        #####
-
         Box2ID = Label(self, text="Course:")
         Box2ID.grid(row=1, column=2, sticky=S)
         
         scroller2 = Scrollbar(self, orient="vertical")
-        Box2 = Listbox(self, width=30, height=20, yscrollcommand=scroller2.set)
-        scroller2.config(command=Box2.yview)
+        self.Box2 = Listbox(self, width=30, height=20, yscrollcommand=scroller2.set)
+        scroller2.config(command=self.Box2.yview)
 
-        Box2.grid(row=2, column=2)
+        self.Box2.grid(row=2, column=2)
         scroller2.grid(sticky=E, row = 2, rowspan = 100, column = 3, ipady = 138)
 
         submitButton = Button(self, text="Submit Course")
         submitButton.grid(row=3, column=2)
         
         #####
+        
+        Box1ID = Label(self, text="Available Courses:")
+        Box1ID.grid(row=1, column=0, sticky=S)
+        
+        scroller1 = Scrollbar(self, orient="vertical")
+        self.Box1 = Listbox(self, width=30, height=20, yscrollcommand=scroller1.set)
+        scroller1.config(command=self.Box1.yview)
+        for course in s.getAllCourses():
+            self.Box1.insert(END, course.name)
+
+        self.Box1.grid(row=2, column=0)
+        scroller1.grid(sticky=E, row = 2, rowspan = 100, column = 1, ipady = 138)
+
+        loginButton = Button(self, text="Choose Course", command = self.chooseCourse)
+        loginButton.grid(row=3, column=0)
+        
+        #####
 
         self.pack()
+
+    def chooseCourse(self):
+        print("Button pressed")
+        selection = self.Box1.curselection()
+        print(selection)
+        if len(selection) > 0:
+            self.Box2.insert(END, self.Box1.get(selection[0]))
+        
         
 def main():
     root = Tk()
